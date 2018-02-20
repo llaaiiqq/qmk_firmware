9bf5509dd7325c3742a0a8f21359b2429c1e60c7
llaaiiqq <curtis.pullen@gmail.com>
2018-02-20 11:56:05 -0800

    more keylload changes

--- keyboards/v60_type_r/keymaps/llaaiiqq/keymap.c
+++ keyboards/v60_type_r/keymaps/llaaiiqq/keymap.c
@@ -21,19 +21,20 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Dvorak */
     [0] = KEYMAP(
-        KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,  XXXXX, KC_BSPC,   \
-        KC_TAB,           KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,   KC_BSLS,          \
-        MO(1),            KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_ENT,                     \
-        KC_LSFT,          XXXXX,   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,  XXXXX,            \
-        KC_LCTL,          KC_LGUI, KC_LALT, KC_SPC,                                               MO(2),   KC_RGUI, KC_RALT, KC_RCTL                    ),
-    /* Keymap 1: FN Layer
-     */
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,      KC_RBRC,       XXXXX,   KC_BSPC, \
+        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,      KC_EQL,        KC_BSLS,          \
+        MO(1),   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,      KC_ENT,                          \
+        KC_LSFT, XXXXX,   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,         KC_RSFT,       XXXXX,            \
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,                                               MO(2),   KC_RGUI, KC_RALT,      KC_RCTL                         ),
+
+    /* Keymap 1: FN Layer */
     [1] = KEYMAP(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _____,  KC_DEL, \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS,          _____, \
-        _____,   _____,   _____,   _____,   _____,   _____, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGUP,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_MUTE, KC_VOLD, KC_VOLU, KC_END,  KC_PGDN,           _____,  _____,  \
-        _____,   _____,   _____,               _____,                                                    _____,   _____,  _____,  _____),
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,        KC_F12,       _____,   KC_DEL,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,   KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, LALT(KC_SLSH), LALT(KC_EQL), _____,            \
+        _____,   _____,   _____,   _____,   _____,   _____,   KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  LALT(KC_MINS), _____,                          \
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,   KC_MUTE, KC_VOLD, KC_VOLU, KC_END,  LCA(KC_Z),     _____,        _____,            \
+        _____,   _____,   _____,   _____,                                                _____,   _____,   _____,         _____                            ),
+
     /* Keymap 3: LED Modification Layer
      * ,-----------------------------------------------------------.
      * |   |BLT|BLC|UGT|UGP|UGB|UGR|   |   |   |   |   |   |       |
